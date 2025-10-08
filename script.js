3426bdc6291d54371ced40de27d735bdbca8e2c3
Ezhilsuresh24 <ezhilsuresh24@gmail.com>
2025-10-08 18:53:51 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function (event) {
-        event.preventDefault();
-        const section = document.querySelector(this.getAttribute('href'));
-        section.scrollIntoView({ behavior: 'smooth' });
-    });
-});
